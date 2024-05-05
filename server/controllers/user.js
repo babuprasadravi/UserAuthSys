fa1834aadf995bb45bcd7b614f36ef180d5a79af
babuprasadravi <babuprasad784@gmail.com>
2024-05-06 00:53:15 +0530

    UPDATE USER API created

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,5 +1,4 @@
 const User = require('../models/user');
-
 // Read user profile
 exports.read = (req, res) => {
   console.log('REQ USER', req);
@@ -22,3 +21,55 @@ exports.read = (req, res) => {
       });
     });
 };
+
+// Update user profile
+exports.update = (req, res, next) => {
+  console.log('UPDATE USER - req.auth', req.auth, 'UPDATE DATA', req.body);
+  const { name, password, phone } = req.body;
+
+  User.findOne({ _id: req.auth._id })
+    .then(user => {
+      if (!user) {
+        return res.status(400).json({
+          error: 'User not found',
+        });
+      }
+
+      if (!name) {
+        return res.status(400).json({
+          error: 'Name is required',
+        });
+      } else {
+        user.name = name;
+      }
+
+      if (phone) {
+        user.phone = phone;
+      }
+
+      if (password && password.length < 6) {
+        return res.status(400).json({
+          error: 'Password should be min 6 characters',
+        });
+      } else if (!password) {
+        // Keep the previous password
+      } else {
+        user.password = password;
+      }
+
+      return user.save();
+    })
+    .then(updatedUser => {
+      // Remove sensitive data from the response
+      updatedUser.hashed_password = undefined;
+      updatedUser.salt = undefined;
+      res.json(updatedUser);
+    })
+    .catch(err => {
+      console.log('USER UPDATE ERROR', err);
+      return res.status(400).json({
+        error: 'User update failed',
+      });
+    });
+};
+
--- server/routes/user.js
+++ server/routes/user.js
@@ -3,11 +3,18 @@ const { sign } = require('jsonwebtoken')
 const router = express.Router()
 
 // Import controller
-const { requireSignin } = require('../controllers/auth')
-const { read } = require('../controllers/user')
+const { requireSignin, adminMiddleware } = require('../controllers/auth')
+const { read,update } = require('../controllers/user')
 
-// Define routes
+// Routes
 
-//Route for reading user profile
+// Route for reading user profile
 router.get('/user/:id', requireSignin, read);
 
+// Route for updating user profile
+router.put('/user/update', requireSignin, update);
+
+// Route for updating admin profile
+router.put('/admin/update', requireSignin, adminMiddleware,  update);
+
+module.exports = router
