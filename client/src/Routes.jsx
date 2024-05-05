372de47e6fd2d0b475868bd8d7b3ada2ad29beeb
babuprasadravi <babuprasad784@gmail.com>
2024-05-06 01:54:44 +0530

    LOGIN FRONTEND

--- client/src/Routes.jsx
+++ client/src/Routes.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Signup from "./auth/Signup";
+import Login from "./auth/Login";
 export default function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Navigate to='/signup' />} />
         <Route path="/signup" element={<Signup />} /> 
+        <Route path="/signin" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
