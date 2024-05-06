c3acefafdf7fa54b10d52cce4d4385703ec7c4f7
babuprasadravi <babuprasad784@gmail.com>
2024-05-06 12:23:10 +0530

    Strong Password Validation

--- client/src/auth/Signup.jsx
+++ client/src/auth/Signup.jsx
@@ -1,10 +1,14 @@
+// Importing necessary dependencies
 import { Link, Navigate } from "react-router-dom";
 import { useState } from "react";
 import styles from "../styles/Signup.module.css";
 import axios from "axios";
-import { Toaster, toast } from 'sonner'
+import { Toaster, toast } from 'sonner';
 import { isAuth } from "./Helpers";
+import { strengthColor, strengthIndicator } from '../util/password-strength';
+import { Box } from '@mui/material';
 
+// Signup component
 const Signup = () => {
     // State for user data and button text
     const [userData, setUserData] = useState({
@@ -16,10 +20,19 @@ const Signup = () => {
         buttonText: "Sign up"
     });
 
+    const [strength, setStrength] = useState(0);
+    const [level, setLevel] = useState();
+
     const { name, email, phone, password, confirmPassword, buttonText } = userData;
 
     // Function to handle input change
     function handleChange(name, event) {
+        if(name === "password"){
+            // Check password strength
+            const temp = strengthIndicator(event.target.value);
+            setStrength(temp);
+            setLevel(strengthColor(temp));
+        }
         setUserData({ ...userData, [name]: event.target.value });
     }
 
@@ -54,24 +67,30 @@ const Signup = () => {
                     confirmPassword: "",
                     buttonText: "Sign up"
                 });
+                setStrength(0);
+                setLevel(strengthColor());
                 toast.success(response.data.message);
             })
             .catch(function (error) {
                 console.log("SIGNUP ERROR", error.response.data);
                 // Reset button text in case of error
-                setUserData({ ...userData, buttonText: "Sign up" });
+                setUserData({ ...userData, password: "", confirmPassword: "", buttonText: "Sign up" });
+                setStrength(0);
+                setLevel(strengthColor());
                 toast.error(error.response.data.error);
             });
     }
 
     return (
         <div>
+            {/* Toaster for displaying notifications */}
             <Toaster richColors position="top-right" expand='true' />
             {/* Redirect to admin dashboard if already authenticated */}
             {isAuth() ? <Navigate to="/admin" /> : null}
             <div className={styles.signup_container}>
                 <div className={styles.signup_form_container}>
                     <div className={styles.left}>
+                        {/* Heading and Login button */}
                         <h1>Welcome Back</h1>
                         <Link to="/signin">
                             <button type="button" className={styles.white_btn}>
@@ -80,8 +99,10 @@ const Signup = () => {
                         </Link>
                     </div>
                     <div className={styles.right}>
+                        {/* Signup form */}
                         <form className={styles.form_container}>
                             <h1>Create Account</h1>
+                            {/* Input fields */}
                             <input
                                 type="text"
                                 placeholder="Name"
@@ -127,6 +148,16 @@ const Signup = () => {
                                 required
                                 className={styles.input}
                             />
+                            {/* Password strength indicator */}
+                            {strength !== 0 && (
+                                <>
+                                    <Box style={{ backgroundColor: level?.color }} sx={{ width: 85, height: 8, borderRadius: '7px' }} className='mt-3 float-left' />
+                                    <h4>
+                                        {level?.label}
+                                    </h4>
+                                </>
+                            )}
+                            {/* Signup button */}
                             <button className={styles.green_btn} onClick={clickSubmit}>
                                 {buttonText}
                             </button>
